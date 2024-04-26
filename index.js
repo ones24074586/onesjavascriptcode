1d3a09f67b23036a40f000499c0eb32dbc2e69f4
onesmus kimaru <kimaruonesmus5@gmail.com>
2024-04-26 10:35:02 +0300

    files added

---
+++ index.js
@@ -0,0 +1,146 @@
+console.log('Hello Onesmus');
+
+// let owner = 'Ones'; // string literal
+// let lastName= 'kimaru';
+// console.log(owner);
+// console.log(lastName);
+
+// let age = 39; //number literal
+// let isapproved = true; //boolean literal
+// let man = undefined; // undefined
+// let color = null;// null can be cleareed later
+// console.log(isapproved); 
+// let theMan = {
+//     name:'ones',
+//     age: 39,
+//     tribe: 'kikuyu' 
+// };
+// console.log(theMan)
+// theMan.name ='kimaru'
+// console.log(theMan.name);
+// let wantedColors = ['white','yellow','orange'];
+// console.log(wantedColors[2]);
+// wantedColors[3]='blue';
+// console.log(wantedColors);
+// let num2 = 4
+// let num3 = 7
+// let name1 ="onesmus"
+// let name2 ="kimaru"
+
+// let joinNames = () =>  {
+//     return name1+name2
+// }
+// console.log(joinNames());
+
+
+
+
+
+// function multiplication(){
+//     let results = num2*num3
+//     return results
+// }
+// console.log(multiplication);
+// let multiplication = () =>{
+//     return
+// }
+//let array1 = [2,4,7,98,40,3,8,7,]
+//.reduce(function(previousValue, currentValue) {
+//     return previousValue + currentValue;
+// }, 0 );
+// console.log("SUM: ", array1 );
+//using reduce method
+//let marks = [1,2,3,4,5]
+//.reduce(function(previousValue, currentValue){
+  //  return previousValue + currentValue;
+// }, 0 );
+
+
+// console.log("SUM: ", marks);
+//using function
+//let marks = [1,2,3,4,5]
+// let summationOfArray = (summation) => {
+//     let total =0;
+//     for(let number of summation){
+//         total += number;
+//     }
+
+//     return total;
+// }
+// console.log(summationOfArray(marks))
+// foreach method
+// let marks = [1,2,3,4,5]
+// let total = 0;
+// marks.forEach((sum) => total += sum)
+// console.log(total);
+
+// let names = ["ones", "ben", "godfrey", "justus"]
+// names.forEach(names=> console.log(names));
+//console.log(names);
+
+// let students = [{name: "onesmus", age: 39},
+// {name: "bon", age: 24},
+// {name: "mwago", age: 36}]
+// //console.log(students);
+
+// // let studentName = students.map(students => students.name);
+// // let studentAge = students.map(students => students.age);
+// // // console.log(studentName);
+// //  console.log(studentAge);
+
+// //let studAge = students.age
+//  //console.log(studAge);
+//  const age = [39, 24, 36];
+ 
+//  let ageMult = age.map(mult)
+//  function mult(ages) {
+//   return ages * 10
+  
+//  }
+//  console.log(ageMult);
+//students.forEach(students=> console.log(students.name))
+// console.log(array1);
+
+// const num1 = "3"
+// let int = Number.parseInt(num1)
+
+// const age = 27
+
+// let sum = int + age
+// console.log(sum);
+let num1 = 15
+
+let num2 = 5
+// 1, subtract num1 minus num2
+let subAnswer = num1 - num2
+console.log("1,minus",+ subAnswer);
+// 2,devide num1 with num 2
+let devAnswer = num1 / num2
+console.log("2,devision", + devAnswer);
+// 3,multiply
+let multAnswer = num1 * num2
+console.log("3,multiplication",+ multAnswer);
+// 4,use modulus operator with num1 and num2
+// modulo is the remainder or what is left after a perfect devision
+// example
+let modulus = num1 % num2
+console.log("4,modulus of num1 devided by num2", + modulus);
+let mod1 = num1 % 4
+console.log("modulus of num1 devided by four", + mod1);
+
+let mod2 = num2 % 3
+console.log("modulus of num2 devided by three", + mod2);
+// const intgNum = num1.includes(number)
+//  const sum = intgNum + age
+//  console.log(sum);
+
+// function arraySum(){
+//     let total = 0;
+//     total += array
+//     return total;
+// }
+// console.log(arraySum().total);
+
+
+
+
